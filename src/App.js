bbb8fd0b0b4d1de38ec9c0792f577ee62fc6f58e
marilena-brink <marilena.brink@gmx.de>
2024-01-05 12:19:32 +0100

    Videoplayer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import VideoPlayer from './VideoPlayer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <VideoPlayer />
   );
 }
 
---
+++ src/VideoPlayer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import * as dashjs from "dash.js";
+export default class VideoPlayer extends React.Component {
+  state = {};
+  componentDidUpdate() {
+      const url = "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd";
+      const video = this.player;
+      const dashjs = dashjs.MediaPlayer().create();
+      dashjs.initialize(video, url, true);
+  }
+  render() {
+    return (
+          <video 
+            ref={player => (this.player = player)}
+            autoPlay={true} 
+          />
+    );
+  }
+}
