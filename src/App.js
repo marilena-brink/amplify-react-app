323b772540208760516d8d672a229cb6519d9f4e
marilena-brink <marilena.brink@gmx.de>
2024-01-25 14:38:53 +0100

    Function for requesting streaming URL

--- src/App.js
+++ src/App.js
@@ -4,10 +4,76 @@ import logo from "./onlyFishLogo.jpg";
 import * as dashjs from "dashjs";
 
 export default function VideoPlayer3() {
+  var AWS = require("aws-sdk/dist/aws-sdk-react-native");
+
+  const axios = require("axios");
+
+  // Erstelle einen Kinesis Video Client
+  const kinesisVideo = new AWS.KinesisVideo({
+    apiVersion: "latest",
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
+    region: "eu-west-1",
+  });
+
+  // Erstelle einen Kinesis Video Archived Media Client
+  let kinesisVideoArchivedMedia;
+
+  // Definiere den Stream Namen
+  const streamName = "OnlyFish"; // Ersetze mit deinem Stream Namen
+
+  // Hole das Endpoint mit GetDataEndpoint
+  kinesisVideo.getDataEndpoint(
+    {
+      APIName: "GET_DASH_STREAMING_SESSION_URL",
+      StreamName: streamName,
+    },
+    (err, response) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // Setze das Endpoint für den Kinesis Video Archived Media Client
+      kinesisVideoArchivedMedia = new AWS.KinesisVideoArchivedMedia({
+        region: "eu-west-1",
+        endpoint: response.DataEndpoint,
+      });
+
+      // Hole die MPEG-DASH URL mit GetDASHStreamingSessionURL
+      kinesisVideoArchivedMedia.getDASHStreamingSessionURL(
+        {
+          StreamName: streamName,
+          DisplayFragmentTimestamp: "ALWAYS",
+        },
+        (err, response) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          // Speichere die MPEG-DASH URL
+          const dashUrl = response.DASHStreamingSessionURL;
+
+          console.log("URL: " + dashUrl);
+
+          // Öffne die URL in einem Media Player deiner Wahl
+          // Zum Beispiel kannst du die URL mit axios anfordern und die Antwort ausgeben
+          axios
+            .get(dashUrl)
+            .then((response) => {
+              console.log(response.data);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+      );
+    }
+  );
+
   const videoRef = useRef(null);
   const playerRef = useRef(null);
   const src =
-    "https://b-d00b5c86.kinesisvideo.eu-west-1.amazonaws.com/dash/v1/getDASHManifest.mpd?SessionToken=CiBa3Gw7w35UYsTxou1Dw321Zy_1UVUGUS7vHNxwZIPdsxIQXNXpcpMnob0Wgrghbp49CBoZ-eoXsd6TdAYsPFU09rmTnbaF80_ngkPg2yIgLuPVcjrsxC_BcibgfoyVo9Q75WhHnV426yuCQrKFIQE~";
+    "https://b-d00b5c86.kinesisvideo.eu-west-1.amazonaws.com/dash/v1/getDASHManifest.mpd?SessionToken=CiCgou2AuwlHlTlyL2r5F30080OXi0ZqGGxEZc5mgGjPDxIQHVD1uth8NnjZldNBZcEv8hoZ5rhB_8T37cq1PWFSXFNsgyDgvAB1Iq08yyIgaTcUuYzrRpW2WjMQnfPEvrpmuA1NhMvZaANlayfIQrs~";
   //"https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
 
   useEffect(() => {
@@ -15,6 +81,18 @@ export default function VideoPlayer3() {
       const video = videoRef.current;
 
       playerRef.current = dashjs.MediaPlayer().create();
+
+      /* restart playback in muted mode when auto playback was not allowed by the browser */
+      playerRef.current.on(
+        dashjs.MediaPlayer.events.PLAYBACK_NOT_ALLOWED,
+        function (data) {
+          console.log(
+            "Playback did not start due to auto play restrictions. Muting audio and reloading"
+          );
+          video.muted = true;
+          playerRef.current.initialize(video, src, true);
+        }
+      );
 
       playerRef.current.initialize(video, src, true);
       playerRef.current.attachView(video);
