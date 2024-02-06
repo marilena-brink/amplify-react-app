a706a31ff3f08c51e5bcd886322c7c003c91f7ee
Teresa Mary Kutzner <teresakutzner@gmail.com>
2024-02-06 17:10:38 +0100

    bucket changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,11 +9,5 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
-  },
-  "storage": {
-    "friendlyImage": {
-      "BucketName": "string",
-      "Region": "string"
-    }
   }
 }
